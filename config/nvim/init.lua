1dc05886e5283cd5b96b33e2783250f2764f6c23
Andres Cuervo <kuerbo@gmail.com>
2025-10-29 19:45:23 +0100

    remove backup,swap and undo files from nvim and vim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,13 +1,28 @@
 -- init.lua — Neovim "VSCode-like" (NVIM 0.11+)
 
+-- move backup, swap and undo files to .cache
+local cache = os.getenv("HOME") .. "/.cache/nvim"
+
+vim.opt.backup = true
+vim.opt.backupext = ".bak"
+vim.opt.backupdir = { cache .. "/backup//" }
+
+vim.opt.swapfile = true
+vim.opt.directory = { cache .. "/swap//" }
+
+vim.opt.undofile = true
+vim.opt.undodir = { cache .. "/undo//" }
+
+-- Leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
 -- Acelera arranque cacheando rquire()
 if vim.loader then
 	vim.loader.enable()
 end
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = ","
-
+-- load configs
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
